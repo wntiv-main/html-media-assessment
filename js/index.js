ecd6ddbc41bcdaecd5451b523785c87518392519
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2022-08-11 13:10:38 +1200

    fade cross images

--- js/index.js
+++ js/index.js
@@ -2,8 +2,35 @@ function randint(/* inclusive */min, /* exclusive */max){
     return min + Math.floor(Math.random() * (max - min));
 }
 
-var games = ["LOL", "MC", "RL"];
-var noImgs = {"LOL": 2, "MC": 2, "RL": 2};
+function getBackgroundImage(){
+    var games = ["LOL", "MC", "RL"];
+    var noImgs = {"LOL": 2, "MC": 2, "RL": 2};
+    var game = games[randint(0, games.length)];
+    return `url("img/${game}_${randint(1, noImgs[game] + 1)}.jpg")`;
+}
 
-var game = games[randint(0, games.length)];
-document.querySelector(".title .background").style.backgroundImage = `url("img/${game}_${randint(1, noImgs[game] + 1)}.jpg")`;
+var background = {
+    main: document.querySelector(".title .background.main"),
+    fade: document.querySelector(".title .background.fade")
+}
+
+background.main.style.backgroundImage = getBackgroundImage();
+
+setInterval(()=>{
+    var image = getBackgroundImage();
+    background.fade.style.backgroundImage = image;
+    setTimeout(()=>{
+        background.fade.style.opacity = 1;
+        setTimeout(()=>{
+            background.main.style.backgroundImage = image;
+            setTimeout(()=>{
+                background.fade.style.opacity = 0;
+            }, 1e3);
+        }, 1e3);
+    }, 1e3);
+}, 10e3);
+
+document.addEventListener("scroll", (e)=>{
+    background.main.style.backgroundPositionY = `${-e.target.scrollTop / 2}px`;
+    background.fade.style.backgroundPositionY = `${-e.target.scrollTop / 2}px`;
+})
