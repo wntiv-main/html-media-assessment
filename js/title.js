ea4faadc2b942c898fe98abe93382c19389fff76
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2022-08-25 19:32:03 +1200

    Update navbar and add navbar items dynamically

--- js/title.js
+++ js/title.js
@@ -2,14 +2,34 @@ function randint(/* inclusive */min, /* exclusive */max){
     return min + Math.floor(Math.random() * (max - min));
 }
 
+function toTitleCase(str) {
+    var result = "", isNewWord = true;
+    for (var char of str) {
+        if (isNewWord && char != char.toUpperCase()) {
+            char = char.toUpperCase();
+            isNewWord = false;
+        }
+        result += char;
+        if (char == ' ') {
+            isNewWord = true;
+        }
+    }
+    return result;
+}
+
+// The games. The URL for the page must be <game>.html and each
+// Image must be <game>_<n>.jpg, where n is in the range 1..noImages[game]
+// and game is an element in the games array.
 var games = ["league-of-legends", "minėcraft", "rocket-league"];
+var navPages = [].concat("index", games);
+var noImgs = { "league-of-legends": 4, "minėcraft": 7, "rocket-league": 2 };
+
 function getBackgroundImage(){
-    var noImgs = {"league-of-legends": 4, "minėcraft": 7, "rocket-league": 2};
     // On main pages, use random game image
     var game = games[randint(0, games.length)];
     // On game-specific pages, use that game's images
     for(var g of games){
-        if(decodeURIComponent(location.pathname).indexOf(g) >= 0){
+        if(decodeURIComponent(location.pathname).includes(g)){
             game = g;
         }
     }
@@ -17,9 +37,11 @@ function getBackgroundImage(){
     return `img/${game}_${randint(1, noImgs[game] + 1)}.jpg`;
 }
 
+// Useful background elements
 var background = {
     main: document.querySelector(".title .background.main"),
-    fade: document.querySelector(".title .background.fade")
+    fade: document.querySelector(".title .background.fade"),
+    header: document.querySelector(".title header")
 }
 
 // Set initial background (hopefully quick enough that user does not notice)
@@ -55,8 +77,10 @@ function doFollowMouse(goto) {
 
 var lastScrollPos = { x: 0, y: 0 };
 function onScroll(e) {
-    target = e.target;
+    var target = e.target;
+    // Find the scrolling element
     while (typeof target.scrollTop == 'undefined') { target = target.lastElementChild; }
+    // Update mouse follower so it doesnt get stuck when scrolling
     var el = document.querySelector(".title .mousefollower");
     var pos = { x: parseFloat(el.style.left), y: parseFloat(el.style.top) }
     pos.x += target.scrollLeft - lastScrollPos.x;
@@ -64,17 +88,39 @@ function onScroll(e) {
     doFollowMouse(pos);
     lastScrollPos.x = target.scrollLeft;
     lastScrollPos.y = target.scrollTop;
-    background.main.style.backgroundPositionY = `${-target.scrollTop / 2}px`;
-    background.fade.style.backgroundPositionY = `${-target.scrollTop / 2}px`;
+    // Update parallax
+    background.main.style.backgroundPositionY = `${-target.scrollTop / 3}px`;
+    background.fade.style.backgroundPositionY = `${-target.scrollTop / 3}px`;
+    background.header.style.top = `calc(50% - ${target.scrollTop * 2 / 3}px)`;
 }
 
 function onMouseMove(e) {
     var el = document.querySelector(".title .mousefollower");
+    // Update mouse follower
     doFollowMouse({
         x: e.pageX - el.getBoundingClientRect().width / 2,
         y: e.pageY - el.getBoundingClientRect().height / 2
     });
 }
 
+// Set up listeners
 document.addEventListener("scroll", onScroll);
 document.addEventListener("mousemove", onMouseMove);
+
+// Set up navbar
+var navbar = document.querySelector("nav ul");
+for (var page of navPages) {
+    var liEl = document.createElement("li");
+    var aEl = document.createElement("a");
+    aEl.innerText = toTitleCase(page.replace(/-/g, " ").replace("index", "home").replace("ė", "e"));
+    aEl.href = `./${page}.html`
+    if (decodeURIComponent(location.pathname).includes(page)) {
+        aEl.className = "selected";
+        // Disable button from navigating
+        aEl.addEventListener("click", (e) => e.preventDefault());
+        // Mark as disabled for screen-readers
+        aEl.setAttribute("aria-disabled", true);
+    }
+    liEl.appendChild(aEl);
+    navbar.appendChild(liEl);
+}
