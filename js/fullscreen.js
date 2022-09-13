6943bdd82511f165c76775bb91d689face136432
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2022-09-13 19:58:18 +1200

    Finish V3 and update documentation

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,3 +1,25 @@
+var fullscreenEls = [];
+
 Array.from(document.getElementsByClassName("imgcontainer")).forEach(el => {
-    el.addEventListener("click", e => el.classList.toggle("fullscreen"));
+    el.addEventListener("click", function handleClick() {
+        for (var i = 0; i < fullscreenEls.length; i++) {
+            var pair = fullscreenEls[i];
+            if (pair.initialEl == el) {
+                var fullscreenEl = pair.fullscreenEl;
+                fullscreenEl.style.opacity = 0;
+                setTimeout(fullscreenEl.remove.bind(fullscreenEl), 300);
+                fullscreenEls.splice(i, 1);
+                return;
+            }
+        }
+        var fullscreenEl = el.cloneNode(true);
+        fullscreenEl.addEventListener("click", handleClick);
+        fullscreenEl.classList.add("fullscreen");
+        document.body.appendChild(fullscreenEl);
+        setTimeout(() => fullscreenEl.style.opacity = 1, 10);
+        fullscreenEls.push({
+            initialEl: el,
+            fullscreenEl: fullscreenEl
+        });
+    });
 })
--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 document.querySelectorAll("a.email").forEach(el => el.setAttribute("href", `mailto:${el.innerText}`));
 
-function onTableScroll(el){
+function onTableScroll(el) {
 	var shadows = [];
-	if(el.scrollWidth - el.clientWidth < 20) {
+	if (el.scrollWidth - el.clientWidth < 20) {
 		el.style.boxShadow = "none";
 		return;
 	}
-	if((el.scrollWidth - el.clientWidth) - el.scrollLeft > 10) {
+	if ((el.scrollWidth - el.clientWidth) - el.scrollLeft > 10) {
 		shadows.push("inset #ffffff80 -50px 0px 50px -50px");
 	}
-	if(el.scrollLeft > 10) {
+	if (el.scrollLeft > 10) {
 		shadows.push("inset #ffffff80 50px 0px 50px -50px");
 	}
 	el.style.boxShadow = shadows.join(", ") || "none";
@@ -18,4 +18,33 @@ document.querySelectorAll("article .tablecontainer").forEach(el => {
 	el.addEventListener("scroll", e => onTableScroll(e.target));
 	window.addEventListener("resize", e => onTableScroll(el));
 	onTableScroll(el);
-})
+});
+
+function scrollToAnchor() {
+	setTimeout(() => {
+		var hash = location.hash.replace("#", "");
+		if (hash) {
+			document.scrollingElement.scrollTo({
+				behavior: "smooth",
+				top: document.getElementById(hash).getBoundingClientRect().top +
+					document.scrollingElement.scrollTop -
+					document.querySelector("nav").getBoundingClientRect().height -
+					20
+			});
+		};
+	}, 100);
+}
+
+document.querySelectorAll("h2").forEach(el => {
+	var section = encodeURIComponent(el.innerText.toLowerCase().replace(/\s/g, "-"));
+	var anchor = document.createElement("a");
+	anchor.innerText = "\u00A7";
+	anchor.id = section;
+	anchor.href = `#${section}`;
+	anchor.title = "Link to this section";
+	anchor.className = "section-link";
+	anchor.addEventListener("click", scrollToAnchor);
+	el.prepend(anchor);
+});
+
+scrollToAnchor();
